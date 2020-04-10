c4db665be8a000738195f6287b0ae510022d9b95
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-04-10 07:50:54 +0100

    Refactor code

--- guessing-game.rb
+++ guessing-game.rb
@@ -1,17 +1,16 @@
-def easyLevel
-  # Easy level. Prompt the user for a number from 1 to 10 and validate. Prompt again if validation fails.
-  puts "You chose easy level. Can you guess the magic number?"
-  puts "Please enter a number between 1 and 10: "
+def gameFunction(guess_max, guess_limit, difficulty)
+  # Easy level. Prompt the user for a number from 1 to guess_max and validate. Prompt again if validation fails.
+  puts "You chose " + difficulty + " level. Can you guess the magic number?"
+  puts "Please enter a number between 1 and " + guess_max.to_s + ":"
   guess = gets.chomp().to_i
-  while guess > 10 or guess < 1
-    puts "You need to enter a number between 1 and 10: "
+  while guess > guess_max or guess < 1
+    puts "You need to enter a number between 1 and " + guess_max.to_s + ":"
     guess = gets.chomp().to_i
   end
 
   # Declare necessary variables
   guess_count = 1
-  guess_limit = 6
-  random_number = rand(1..10)
+  random_number = rand(1..guess_max)
   out_of_guesses = false
   
   while guess != random_number and !out_of_guesses
@@ -21,8 +20,8 @@ def easyLevel
       puts "That was wrong."
       puts "You have " + (guess_limit - (guess_count)).to_s + " guesses left. Guess again: "
       guess = gets.chomp().to_i
-      while guess > 10 or guess < 1
-        puts "You need to enter a number between 1 and 10: "
+      while guess > guess_max or guess < 1
+        puts "You need to enter a number between 1 and " + guess_max.to_s + ":"
         guess = gets.chomp().to_i
       end
       guess_count += 1
@@ -33,79 +32,6 @@ def easyLevel
   #pass out of guesses value to function
   checkForWin(out_of_guesses)
 end
-
-def mediumLevel
-  # Medium level. Prompt the user for a number from 1 to 20 and validate. Prompt again if validation fails.
-  puts "You chose medium level. Can you guess the magic number?"
-  puts "Please enter a number between 1 and 20: "
-  guess = gets.chomp().to_i
-  while guess > 20 or guess < 1
-    puts "You need to enter a number between 1 and 20: "
-    guess = gets.chomp().to_i
-  end
-
-  # Declare necessary variables
-  guess_count = 1
-  guess_limit = 4
-  random_number = rand(1..20)
-  out_of_guesses = false
-  
-  while guess != random_number and !out_of_guesses
-    # check if the user guesses correctly the random number and if out of guesses
-    if guess_count < guess_limit
-      # Ask the user to guess again and run validation on user prompt
-      puts "That was wrong."
-      puts "You have " + (guess_limit - (guess_count)).to_s + " guesses left. Guess again: "
-      guess = gets.chomp().to_i
-      while guess > 20 or guess < 1
-        puts "You need to enter a number between 1 and 20: "
-        guess = gets.chomp().to_i
-      end
-      guess_count += 1
-    else
-      out_of_guesses = true
-    end
-  end
-  #pass out of guesses value to function
-  checkForWin(out_of_guesses)
-end
-
-def hardLevel
-  # Hard level. Prompt the user for a number from 1 to 50 and validate. Prompt again if validation fails.
-  puts "You chose hard level. Can you guess the magic number?"
-  puts "Please enter a number between 1 and 50: "
-  guess = gets.chomp().to_i
-  while guess > 50 or guess < 1
-    puts "You need to enter a number between 1 and 50: "
-    guess = gets.chomp().to_i
-  end
-
-  # Declare necessary variables
-  guess_count = 1
-  guess_limit = 3
-  random_number = rand(1..50)
-  out_of_guesses = false
-  
-  while guess != random_number and !out_of_guesses
-    # check if the user guesses correctly the random number and if out of guesses
-    if guess_count < guess_limit
-      # Ask the user to guess again and run validation on user prompt
-      puts "That was wrong."
-      puts "You have " + (guess_limit - (guess_count)).to_s + " guesses left. Guess again: "
-      guess = gets.chomp().to_i
-      while guess > 50 or guess < 1
-        puts "You need to enter a number between 1 and 50: "
-        guess = gets.chomp().to_i
-      end
-      guess_count += 1
-    else
-      out_of_guesses = true
-    end
-  end
-  #pass out of guesses value to function
-  checkForWin(out_of_guesses)
-end
-
 
 
 def checkForWin(out_of_guesses)
@@ -120,11 +46,11 @@ end
 def checkLevel(level)
   # Check level the user wants and call the appropriate function
   if level == "easy"
-    easyLevel()
+    gameFunction(10, 6, level)
   elsif level == "medium"
-    mediumLevel()
+    gameFunction(20, 4, level)
   elsif level == "hard"
-    hardLevel()
+    gameFunction(50, 3, level)
   end
 end
 
